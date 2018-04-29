4a043ccc3cde7d96529afbffc54ed40af18a6c4e
dmitry-moiseenko <atomicyear@gmail.com>
2018-04-29 22:23:12 +0300

    contin

---
+++ src/BasicOOP.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Dmitriy on 4/29/2018.
+ */
+public class BasicOOP {
+    public static void main(String[] args) {
+        System.out.println("BasicOOP");
+        System.out.println("contin");
+    }
+}
