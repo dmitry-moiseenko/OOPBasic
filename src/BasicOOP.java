5f9426e3939f6d699d84eb0c843c950232da5734
dmitry-moiseenko <atomicyear@gmail.com>
2018-04-29 22:33:09 +0300

    test

--- src/BasicOOP.java
+++ src/BasicOOP.java
@@ -1,9 +1,8 @@
-/**
- * Created by Dmitriy on 4/29/2018.
- */
+
 public class BasicOOP {
     public static void main(String[] args) {
         System.out.println("BasicOOP");
         System.out.println("contin");
+        System.out.println("test");
     }
 }
