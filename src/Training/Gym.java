bb0c921fdd9f2e4130bc7275ec1d031ae50e95d0
dmitry-moiseenko <atomicyear@gmail.com>
2018-04-30 11:54:34 +0300

    Создан зал

---
+++ src/Training/Gym.java
@@ -0,0 +1,13 @@
+package Training;
+
+public class Gym {
+    private String name;
+
+    Gym(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+}
---
+++ src/Training/Main.java
@@ -0,0 +1,10 @@
+package Training;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Gym gym = new Gym("Industrialny");
+        System.out.println(gym);
+
+    }
+}
