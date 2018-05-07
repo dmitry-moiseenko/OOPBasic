c9a225c3526e6c13e35ff6b7ecd4806e82399aa3
dmitry-moiseenko <atomicyear@gmail.com>
2018-05-08 00:26:07 +0300

    LAB24

---
+++ src/LAB24/Main.java
@@ -0,0 +1,12 @@
+package LAB24;
+
+/**
+ * Created by Dmitriy on 5/8/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Issue issue = new Issue("TestNameIssue", 100);
+        System.out.println(issue.toPrint());
+
+    }
+}
